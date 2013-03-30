45d0b01206b31561c92dae2a6293d6cf6de13674
Priscilla Mok <priscillamok@gmail.com>
2013-03-30 13:32:28 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookMaster::Application.config.secret_token = '5817169d7e5b11d023a37f946c80b6edef3be4e36a0de77754b4a1ab16640b67740c059894f2e0765527248b65a71a8ef1a137cd215e1de2bb52edbc54903131'
